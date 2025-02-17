737e26d470f42fd361a4fbcde9f55463b80d2a10
jhoao-santos <fjoao1020@gmail.com>
2025-02-17 15:15:57 -0300

    Primeiro programa

---
+++ src/secao01/exercicio01.java
@@ -0,0 +1,9 @@
+package secao01;
+
+public class exercicio01 {
+    public static void main(String[] args) {
+        System.out.println("Ola, Mundo! Este e o meu primeiro programa em Java! ");
+
+        System.out.println("Meu  nome e joao! ");
+    }
+}
