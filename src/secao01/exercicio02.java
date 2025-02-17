f518a3663a3524921f01d166e8cf5fc580b4e9c0
jhoao-santos <fjoao1020@gmail.com>
2025-02-17 17:42:56 -0300

    exercicio02

---
+++ src/secao01/exercicio02.java
@@ -0,0 +1,23 @@
+package secao01;
+
+public class exercicio02 {
+    public static void main(String[] args) {
+        
+
+        /*
+         * Crie um programa que declare e inicialize variáveis de todos os 
+         * tipos primitivos em Java (int,
+        double, char, boolean, etc.). Exiba o valor de cada variável no console
+         */
+
+         int inteiro = 10;
+         double decimal = 10.5;
+         char caractere = 'a';
+         boolean boleano = true;
+
+         System.out.println("Inteiro: " + inteiro);
+         System.out.println("Decimal: " + decimal);
+         System.out.println("Caractere: " + caractere);
+         System.out.println("Booleano: " + boleano);
+    }
+}
